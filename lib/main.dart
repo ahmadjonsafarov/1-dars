8866bb006277ee5e5b60b12457a3521bfcfa4f49
Ahmadjon Safarov <ahmadjonsafarov1112@gmail.com>
2022-11-08 10:27:19 +0500

    nimadur

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,79 @@
+import 'package:flutter/material.dart';
+
 void main(){
-  print('nimadur');
+
+  runApp(const MyApp());
+
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+          color: Colors.black,
+          child: Column(
+            mainAxisAlignment:  MainAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(20),topRight: Radius.circular(20))
+                ),
+                child: Center(
+                  child: Text('R',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      color: Colors.black,
+                    ),),
+                ),
+                height: 200,
+                width: 500,
+
+              ),
+
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.all(Radius.circular(0))
+                ),
+                child: Center(
+                  child: Text('U',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      color: Colors.black,
+                    ),),
+                ),
+                height: 200,
+                width: 500,
+
+              ),
+
+              Container(
+                decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20),bottomRight: Radius.circular(20))
+                ),
+                child: Center(
+                  child: Text('S',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      decoration: TextDecoration.none,
+                      color: Colors.black,
+                    ),),
+                ),
+                height: 200,
+                width: 500,
+
+              )
+            ],
+          )
+      ),
+    );
+  }
+
 }
